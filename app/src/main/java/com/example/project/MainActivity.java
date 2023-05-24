0c08450d56e83ee93de62853db63747bc9ea1ef0
Axel <a22axeju@student.his.se>
2023-05-24 16:57:33 +0200

    Uppdaterade "MainActivity" med bland annat button click listener och JSON datahämtning.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,89 @@
 package com.example.project;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<Elephants> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Button myButton = findViewById(R.id.buttonAbout);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to launch the about activity
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+
+                // Launch the about activity
+                startActivity(intent);
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Call the method to fetch JSON data and populate the RecyclerView
+        fetchElephantsData();
     }
+
+    private void fetchElephantsData() {
+        String jsonUrl = "https://mobprog.webug.se/json-api?login=a22axeju";
+
+        JsonTask jsonTask = new JsonTask(this);
+        jsonTask.execute(jsonUrl);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Elephants>>() {}.getType();
+        items = gson.fromJson(json, type);
+        setRecyclerViewData(items);
+    }
+
+    private void setRecyclerViewData(ArrayList<Elephants> elephantsArrayList) {
+        recyclerViewAdapter = new RecyclerViewAdapter(elephantsArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_about) {
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
